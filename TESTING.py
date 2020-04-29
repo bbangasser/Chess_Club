9aff936bdde897e2e3b29c8f2228986c5118a332
stephenkasahara <sykasahara@students.unwsp.edu>
2020-04-29 13:05:05 -0500

    Main Menu Additions

--- TESTING.py
+++ TESTING.py
@@ -1,6 +1,7 @@
 import tkinter as tk
 
-LARGE_FONT = ("Verdana",12) 
+LARGE_FONT = ("Verdana", 12)
+
 
 class ChessProgram(tk.Tk):
     def __init__(self):
@@ -15,31 +16,109 @@ class ChessProgram(tk.Tk):
         self._frame = new_frame
         self._frame.pack()
 
+    # exit frame
+    def delete_frame(self):
+        self._frame.destroy()
+        app.destroy()
+
+
+# Main Menu
 class StartPage(tk.Frame):
     def __init__(self, master):
         tk.Frame.__init__(self, master)
-        tk.Label(self, text="Chess Club Program", font = LARGE_FONT).pack(side="top", fill="x", pady=10)
-        
-        tk.Button(self, text="Open page one",
-                  command=lambda: master.switch_frame(PageOne)).pack()
-        tk.Button(self, text="Open page two",
-                  command=lambda: master.switch_frame(PageTwo)).pack()
-        tk.Button(self, text="Open page three",
-                  command=lambda: master.switch_frame(PageTwo)).pack()
+        tk.Label(self, text="Chess Club Program", font=LARGE_FONT).pack(side="top", fill="x", pady=10)
 
+        tk.Button(self, text="Record a Match",
+                  command=lambda: master.switch_frame(PageOne)).pack(fill="both", pady=3)
+        tk.Button(self, text="Add a Player",
+                  command=lambda: master.switch_frame(PageTwo)).pack(fill="both", pady=3)
+        tk.Button(self, text="Delete Player",
+                  command=lambda: master.switch_frame(PageThree)).pack(fill="both", pady=3)
+        tk.Button(self, text="LeaderBoard",
+                  command=lambda: master.switch_frame(PageFour)).pack(fill="both", pady=3)
+        tk.Button(self, text="Exit",
+                  command=lambda: master.delete_frame()).pack(fill="both", pady=3)
+
+
+# Recording a match
 class PageOne(tk.Frame):
     def __init__(self, master):
         tk.Frame.__init__(self, master)
-        tk.Label(self, text="This is page one").pack(side="top", fill="x", pady=10)
-        tk.Button(self, text="Return to start page",
-                  command=lambda: master.switch_frame(StartPage)).pack()
 
+        # defining variables
+        self.player1ID = tk.StringVar()
+        self.player2ID = tk.StringVar()
+
+        tk.Label(self, text="Record the Match").grid(column=1, row=0, sticky=tk.N)
+
+        # Create text entries for players
+        tk.Label(self, text="Player One ID").grid(column=0, row=1, sticky=tk.E)
+        tk.Entry(self, width=25, textvariable=self.player1ID).grid(column=1, row=1)
+        tk.Label(self, text="Player Two ID").grid(column=0, row=2, sticky=tk.E)
+        tk.Entry(self, width=25, textvariable=self.player2ID).grid(column=1, row=2)
+
+        # Create a enterButton
+        tk.Button(self, text="Enter", command=self.enter).grid(column=1, row=4)
+
+        # Create ExitButton
+        tk.Button(self, text="Return to start page",
+                  command=lambda: master.switch_frame(StartPage)).grid(column=0, row=4)
+
+        for child in self.winfo_children():
+            child.grid_configure(padx=5, pady=3)
+
+    # Enters the values into the database
+    def enter(self):
+        print(self.player1ID.get(), " was entered!")
+        print(self.player2ID.get(), " was entered!")
+
+
+# Add player
 class PageTwo(tk.Frame):
     def __init__(self, master):
         tk.Frame.__init__(self, master)
-        tk.Label(self, text="This is page two").pack(side="top", fill="x", pady=10)
+        tk.Label(self, text="Add Player").pack(side="top", fill="x", pady=10)
         tk.Button(self, text="Return to start page",
                   command=lambda: master.switch_frame(StartPage)).pack()
+
+
+# Delete player
+class PageThree(tk.Frame):
+    def __init__(self, master):
+        tk.Frame.__init__(self, master)
+        tk.Label(self, text="Delete Player").grid(column=1, row=0, sticky=tk.N)
+
+        # define variable
+        self.deletedPlayerID = tk.StringVar()
+
+        # Create a Label and Text Entry for player ID
+        tk.Label(self, text="Player's ID").grid(column=0, row=1, sticky=tk.W, columnspan=1)
+        tk.Entry(self, width=25, textvariable=self.deletedPlayerID).grid(column=1, row=1)
+
+        # Create a DeleteButton
+        tk.Button(self, text="Enter", command=self.deleted).grid(column=1, row=4)
+
+        # Create ExitButton
+        tk.Button(self, text="Return to start page",
+                  command=lambda: master.switch_frame(StartPage)).grid(column=0, row=4)
+
+        for child in self.winfo_children():
+            child.grid_configure(padx=5, pady=3)
+
+    # Deletes player from database
+    def deleted(self):
+        print(self.deletedPlayerID.get(), "was deleted!")
+        self.deletedPlayerID.set("")
+
+
+# LeaderBoard
+class PageFour(tk.Frame):
+    def __init__(self, master):
+        tk.Frame.__init__(self, master)
+        tk.Label(self, text="LeaderBoard").pack(side="top", fill="x", pady=10)
+        tk.Button(self, text="Return to start page",
+                  command=lambda: master.switch_frame(StartPage)).pack()
+
 
 if __name__ == "__main__":
     app = ChessProgram()
